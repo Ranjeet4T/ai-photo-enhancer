76c2d4888abd21ba749ad6fdfba7be466eff8c69
Ranjeet4T <patelramayana5@gmail.com>
2026-04-21 13:59:23 +0530

    Delete api directory

--- api/login.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const { email, password } = req.body;
-
-  if (email === "admin@gmail.com" && password === "1234") {
-    res.status(200).json({ success: true });
-  } else {
-    res.status(200).json({ success: false });
-  }
-}
